c96a16863348fb06e6d7e54e242f6d18538202dd
Ian <idmooney@mavs.colradomesa.edu>
2025-05-05 08:25:01 -0600

    adding Rabin as a .h

--- project/Rabin.h
+++ project/Rabin.h
@@ -1,16 +1,15 @@
 /* Following program is a C++ implementation of Rabin Karp
 Algorithm given in the CLRS book */
+#pragma once
 #include <chrono>
 #include <iostream>
 #include <string>
 #include "../Open Hashing/Functions.h"
 using namespace std;
 
-int search(string, string, int);
+int search(string pat, string txt, int q);
 bool Las_vegas(string, string, int);
 int Wordplace(int, string);
-/* 
-For when we add all these togeather, replace the name main with this:
 
 int Rabin(int row)
 {
@@ -23,30 +22,16 @@ int Rabin(int row)
     clean(pat);
 
     word_count = search(pat, txt, q);
-    if(amount > 0 )
+    
+    if(word_count > 0 )
     {
     cout << " Pattern found on row: "<< row << endl;
     }
     cout << "There are a total of " << word_count << " in the txt that match" << endl;
     return 0;
 
-    Then modify the search to this search(string pat, string txt, int q, int row);
 }
-*/
-int main()
-{
-    string txt = "how much wood could a wood chuck chuck if a wood chuck could chuck wood";
-    string pat = "wood";
-    int q = INT_MAX;
-    int word_count;
 
-    clean(txt);
-    clean(pat);
-
-    word_count = search(pat, txt, q);
-    cout << "There are a total of " << word_count << " in the txt that match" << endl;
-    return 0;
-}
 
 // Search the pat string in the txt string
 int search(string pat, string txt, int q)
@@ -54,10 +39,10 @@ int search(string pat, string txt, int q)
     int M = pat.size();
     int N = txt.size();
     int i, j;
-    int p = 0; // hash value for pattern
-    int t = 0; // hash value for txt
+    int pattern = 0; // hash value for pattern
+    int text = 0; // hash value for txt
     int h = 1;
-    int d = 256; // d is the number of characters in the input alphabet
+    int d = 256; // d is the number of characters in the input ASCII alphabet
     int amount = 0;
 
     /* start the clock for run time of both Rabin Carp and Las Vegas check */
@@ -72,17 +57,15 @@ int search(string pat, string txt, int q)
     /* Calculate hash values of pattern and first window of text */
     for (i = 0; i < M; i++)
     {
-        p = (d * p + pat[i]) % q;
-        t = (d * t + txt[i]) % q;
+        pat = (d * pattern + pat[i]) % q;
+        text = (d * text + txt[i]) % q;
     }
 
     /* Slide the pattern over text one by one */
     for (i = 0; i <= N - M; i++)
     {
-
-        if (p == t)
+        if (pattern == text)
         {
-
             /* Check for characters one by one */
             for (j = 0; j < M; j++)
             {
@@ -122,19 +105,16 @@ int search(string pat, string txt, int q)
                     auto tot_stop = std::chrono::high_resolution_clock::now();
                     auto tot_time = std::chrono::duration_cast<std::chrono::nanoseconds>(tot_stop - tot_start);
                     cout << "Pattern denied by Las Vegas check" << endl;
-
                     std::cout << "Rabin Carp time: " << tot_time.count() << " nanoseconds!" << std::endl;
                 }
             }
         }
-        // Calculate hash value for next window of text:
-        // Remove leading digit, add trailing digit
         if (i < N - M)
         {
-            t = (d * (t - txt[i] * h) + txt[i + M]) % q;
-            if (t < 0)
+            text = (d * (text - txt[i] * h) + txt[i + M]) % q;
+            if (text < 0)
             {
-                t = (t + q);
+                text = (text + q);
             }
         }
     }
